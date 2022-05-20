c38a77fef31ccd3a3ca0ad1c020fc7958121a8d3
vypnt <vypnt@rever.vn>
2022-05-20 10:31:46 +0700

    Test commit

---
+++ frond-end-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div><h1>{{tasks}}</h1></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "App",
+ data() {
+   return {
+     tasks: "Hello Rever"
+   };
+ },
+})
+</script>
+
+
+<style >
+h1 { 
+  color: red;
+}
+</style>
