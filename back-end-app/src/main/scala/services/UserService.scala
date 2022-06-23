bb795d3ae6465bc19ce6fb688915d67485079dd8
tongthanhkt <64117066+tongthanhkt@users.noreply.github.com>
2022-06-23 11:56:17 +0700

    search user

---
+++ back-end-app/src/main/scala/services/UserService.scala
@@ -0,0 +1,33 @@
+package services
+
+import com.twitter.util.jackson.JSON
+import models._
+import net.liftweb.json.{DefaultFormats, parse}
+import utils.DatabaseConnection
+
+import java.sql.SQLException
+import java.util
+import javax.inject.Inject
+
+class UserService  @Inject()(databaseConnection:DatabaseConnection) {
+  def searchUser(searchUserRequest:SearchUserRequest):util.ArrayList[User]={
+    val userList = new util.ArrayList[User]()
+    val sql =
+      """
+        |SELECT * from user as us
+        |where (? is null or us.username LIKE CONCAT('%',?,'%') or us.fullname LIKE CONCAT('%',?,'%'));
+        |""".stripMargin
+    val con = databaseConnection.getConnection()
+    val pst = con.prepareStatement(sql)
+    pst.setString(1,searchUserRequest.keyword);
+    pst.setString(2,searchUserRequest.keyword);
+    pst.setString(3,searchUserRequest.keyword);
+    val rs = pst.executeQuery
+    while(rs.next){
+      val e = User(username=rs.getString("username"),fullname = rs.getString("fullname"))
+      userList.add(e)
+    }
+    con.close()
+    return userList;
+  }
+}
