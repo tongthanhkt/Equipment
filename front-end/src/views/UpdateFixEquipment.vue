1bebb7d120663bc969124172639a53b77bc84100
tongthanhkt <64117066+tongthanhkt@users.noreply.github.com>
2022-07-04 16:04:32 +0700

    fix

--- front-end/src/views/UpdateFixEquipment.vue
+++ front-end/src/views/UpdateFixEquipment.vue
@@ -8,7 +8,7 @@
       <h1
         class="px-2 pt-2 pb-1 col-span-3 text-lg font-medium text-white w-auto"
       >
-        Chỉnh sửa thông tin bàn giao - {{ id }}
+        Sửa chữa thiết bị
       </h1>
       <button
         class="place-self-end bg-indigo-500 hover:bg-indigo-200 m-2 transition-colors w-auto text-white rounded-md focus:outline-none"
@@ -34,12 +34,12 @@
           <div
             class="mx-1 px-2 py-1.5 border bg-gray-200 focus:ring-gray-500 w-11/12 hover:border-gray-900 lg:text-base sm:text-sm border-gray-300 rounded focus:outline-none text-gray-700"
           >
-            {{ record.name }}
+            {{ record.equipment_name }}
           </div>
         </div>
         <div class="p-1 font-medium text-gray-700">Chi phí</div>
         <div class="p-1 col-span-2 font-medium text-gray-700">
-          Thời gian bàn giao
+          Thời gian sửa chữa
         </div>
 
         <div>
@@ -58,21 +58,15 @@
           />
         </div>
 
-        <div class="p-1 font-medium text-gray-700">Người bàn giao</div>
+        <div class="p-1 font-medium text-gray-700">Người sửa chữa</div>
         <div class="pl-1 col-span-2 font-medium text-gray-700">
-          Loại bàn giao
+          Trạng thái sửa chữa
         </div>
         <div>
-          <!-- <input
-                type="text"
-                class=" mx-1  px-2 py-1.5 border focus:ring-gray-500 w-11/12 hover:border-gray-900 lg:text-base sm:text-sm border-gray-300 rounded focus:outline-none text-black"
-                placeholder=""
-                v-model="record.take_over_person"
-              /> -->
           <v-select
             class="mx-1 bg-white border focus:ring-gray-500 w-11/12 hover:border-gray-900 lg:text-sm sm:text-sm border-gray-300 rounded focus:outline-none text-black"
             :options="options"
-            v-model="take_over_person"
+            v-model="fixer"
             :get-option-label="(option) => option.username"
             :dropdown-should-open="dropdownShouldOpen"
           >
@@ -93,67 +87,16 @@
         </div>
         <div class="col-span-2">
           <select
-            v-model="record.type_take_over"
+            v-model="record.status"
             id="type"
             name="type"
             autocomplete="type-name"
             class="mx-1 px-2 py-1.5 border focus:ring-gray-500 w-11/12 hover:border-gray-900 lg:text-base sm:text-sm border-gray-300 rounded-md focus:outline-none text-black"
           >
-            <option value="1">Bàn giao thiết bị mới</option>
-            <option value="2">Bàn giao thiết bị sau khi sửa chữa</option>
+            <option value="-1">Không sửa được</option>
+            <option value="0">Đang sửa</option>
+            <option value="1">Sửa thành công</option>
           </select>
-        </div>
-        <div class="p-1 font-medium text-gray-700">Người nhận thiết bị</div>
-        <div class="p-1 col-span-2 font-medium text-gray-700">
-          Người xác nhận
-        </div>
-
-        <div>
-          <v-select
-            class="mx-1 bg-white border focus:ring-gray-500 w-11/12 hover:border-gray-900 lg:text-sm sm:text-sm border-gray-300 rounded focus:outline-none text-black"
-            :options="options"
-            v-model="user"
-            :get-option-label="(option) => option.username"
-            :dropdown-should-open="dropdownShouldOpen"
-            @change="changeUser"
-          >
-            <template #search="{ attributes, events }">
-              <input
-                class="vs__search bg-white lg:text-base sm:text-sm focus:outline-none text-black"
-                v-bind="attributes"
-                v-on="events"
-                @input="retrieveUser"
-              />
-            </template>
-            <template #option="{ username, fullname }">
-              {{ fullname }}
-              <br />
-              <cite>{{ username }} </cite>
-            </template>
-          </v-select>
-        </div>
-        <div>
-          <v-select
-            class="mx-1 bg-white border focus:ring-gray-500 w-11/12 hover:border-gray-900 lg:text-sm sm:text-sm border-gray-300 rounded focus:outline-none text-black"
-            :options="options"
-            v-model="verifier"
-            :get-option-label="(option) => option.username"
-            :dropdown-should-open="dropdownShouldOpen"
-          >
-            <template #search="{ attributes, events }">
-              <input
-                class="vs__search bg-white lg:text-base sm:text-sm focus:outline-none text-black"
-                v-bind="attributes"
-                v-on="events"
-                @input="retrieveUser"
-              />
-            </template>
-            <template #option="{ username, fullname }">
-              {{ fullname }}
-              <br />
-              <cite>{{ username }} </cite>
-            </template>
-          </v-select>
         </div>
       </div>
       <div
@@ -167,35 +110,17 @@
           cols="50"
           class="mx-1 my-2 px-2 py-1.5 border rounded"
           v-model="record.message"
-        >
-        </textarea>
+        ></textarea>
         <div class="pl-1 font-medium text-gray-700">Tệp đính kèm</div>
-        <div v-if="currentFileName.length != 0">
-          <ul class="list-group list-group-flush flex flex-row flex-wrap">
-            <div v-for="(file_name, index) in currentFileName" :key="index">
-              <div>
-                <div
-                  class="bg-gray-300 w-fit h-fit border rounded flex flex-row m-2"
-                >
-                  <fa icon="file-arrow-up" class="px-2 py-2"></fa>
-                  <div class="py-1">{{ file_name }}</div>
-                  <span
-                    class="close px-2 py-1"
-                    @click="deleteCurrentFile(index)"
-                    >&times;</span
-                  >
-                </div>
-              </div>
-            </div>
-          </ul>
-        </div>
+        <!-- <div class="mx-2 bg-gray-50 w-full h-24 my-2"> 
 
+       </div> -->
         <div class="mx-1 mt-2 mb-3">
           <div class="row">
-            <div class="col-8">
-              <label class="px-2">Thêm tệp mới: </label>
-
-              <input type="file" ref="file" @change="selectFiles" multiple />
+            <div class="col-5">
+              <label class="btn btn-default p-0">
+                <input type="file" ref="file" @change="selectFiles" multiple />
+              </label>
             </div>
           </div>
           <span
@@ -208,13 +133,13 @@
 
           <div class="bg-white h-36 overflow-y-auto border-2 border-indigo-300">
             <div
-              class="border-b-2 border-indigo-300 text-sm text-orange-600 font-semibold flex flex-row"
+              class="border-b-2 border-indigo-300 text-base text-orange-600 font-semibold flex flex-row"
             >
               <fa icon="folder" class="px-2 py-1"></fa>
-              <div class="p-1">Selected Files</div>
+              <div>Selected Files</div>
             </div>
             <ul class="list-group list-group-flush flex flex-row flex-wrap">
-              <div v-for="(file, index) in allFiles">
+              <div v-for="(file, index) in allFiles" :key="index">
                 <div>
                   <div
                     class="bg-gray-300 w-fit h-fit border rounded flex flex-row m-2"
@@ -238,11 +163,11 @@
       <!-- <div class="p-1 m-2 text-base  font-medium text-gray-700">Thông tin</div> -->
       <div class="flex flex-row gap">
         <button
-          class="bg-sky-500 hover:bg-sky-600 m-3.5 transition-colors text-base w-auto text-white p-2 rounded-md focus:outline-none"
-          @click="updateTakeOverRecord"
+          class="bg-green-500 hover:bg-green-600 m-3.5 transition-colors text-base w-auto text-white p-2 rounded-md focus:outline-none"
+          @click="updateFixEquipmentRecord"
         >
-          <fa icon="pen-to-square" class="px-1"></fa>
-          Save
+          <fa icon="rotate-right" class="px-1"></fa>
+          Cập nhật sửa chữa
         </button>
         <button
           class="bg-red-500 hover:bg-red-600 m-3.5 transition-colors w-auto text-white p-2 rounded-md focus:outline-none"
@@ -255,13 +180,12 @@
     </div>
   </div>
 </template>
-
 <script lang="ts">
 import Datepicker from "@vuepic/vue-datepicker";
 import { ref } from "vue";
 import UploadService from "../services/equipments/UploadFilesService";
-import TakeOverService from "@/services/takeover/TakeOverService";
-import TakeOverRecord from "@/types/TakeOverRecord";
+import FixEquipmentService from "@/services/fixEquipment/FixEquipmentService";
+import FixEquipmentRecord from "@/types/FixEquipmentRecord";
 import { Vue, Options, Prop, Emit, Ref } from "vue-property-decorator";
 import "vue-select/dist/vue-select.css";
 import Fuse from "fuse.js";
@@ -274,16 +198,15 @@ import UploadFilesService from "../services/equipments/UploadFilesService";
     Datepicker,
   },
 })
-export default class UpdateTakeOver extends Vue {
-  record: TakeOverRecord = {
+export default class UpdateFixEquipment extends Vue {
+  record: FixEquipmentRecord = {
     id: "",
     equipment_id: "",
-    username: "",
-    take_over_time: "",
+    device_id: "",
+    equipment_name: "",
+    fixing_time: "",
     status: "",
-    verifier: "",
-    take_over_person: "",
-    type_take_over: "",
+    fixer: "",
     message: null,
     cost: null,
     created_by: "",
@@ -291,21 +214,12 @@ export default class UpdateTakeOver extends Vue {
     updated_by: "",
     updated_time: "",
     metadata_info: "",
-    device_id: "",
     name: "",
   };
   @Prop() id!: number;
   options: User[] = [];
-  user: User = {
-    username: this.record.username,
-    fullname: "",
-  };
-  take_over_person: User = {
-    username: this.record.take_over_person,
-    fullname: "",
-  };
-  verifier: User = {
-    username: this.record.verifier,
+  fixer: User = {
+    username: this.record.fixer,
     fullname: "",
   };
   timeOut: any;
@@ -313,7 +227,7 @@ export default class UpdateTakeOver extends Vue {
   currentMetaData: any;
   currentFileName: string[] = [];
 
-  @Emit("changeUpdateTakeOverShow")
+  @Emit("changeUpdateFixEquipmentShow")
   changeShow(data: boolean) {
     return data;
   }
@@ -359,15 +273,10 @@ export default class UpdateTakeOver extends Vue {
     }
     return obj;
   }
-  changeUser() {
-    this.verifier = this.user;
-  }
 
   async created() {
     await this.retrieveRecord();
-    this.options.push(this.user);
-    this.options.push(this.take_over_person);
-    this.options.push(this.verifier);
+    this.options.push(this.fixer);
   }
 
   async getCurrentMetaData() {
@@ -404,7 +313,7 @@ export default class UpdateTakeOver extends Vue {
   }
 
   async retrieveRecord() {
-    await TakeOverService.getRecordById(this.id)
+    await FixEquipmentService.getRecordById(this.id)
       .then((res) => {
         this.record = res.data;
         console.log(this.record);
@@ -413,7 +322,7 @@ export default class UpdateTakeOver extends Vue {
         // );
         if (this.record.cost != null)
           this.record.cost = parseFloat(this.record.cost).toString();
-        this.editDate = ref(new Date(Number(this.record.take_over_time)));
+        this.editDate = ref(new Date(Number(this.record.fixing_time)));
         this.currentMetaData = Object.entries(res.data.metadata_info);
         let result = Object.values(res.data.metadata_info).map(
           (File: any) => File.file_name
@@ -422,16 +331,9 @@ export default class UpdateTakeOver extends Vue {
           this.currentFileName[index] = `${file_name}`;
         });
         console.log(this.editDate);
-        this.user = {
-          username: this.record.username,
-          fullname: "",
-        };
-        this.take_over_person = {
-          username: this.record.take_over_person,
-          fullname: "",
-        };
-        this.verifier = {
-          username: this.record.verifier,
+
+        this.fixer = {
+          username: this.record.fixer,
           fullname: "",
         };
       })
@@ -446,22 +348,15 @@ export default class UpdateTakeOver extends Vue {
     )
       return "";
     var d = new Date((event.target as HTMLInputElement).value);
-    this.record.take_over_time = d.toLocaleString();
+    this.record.fixing_time = d.toLocaleString();
   }
 
-  async updateTakeOverRecord() {
-    if (this.user == null) {
-      alert("Hãy chọn người nhận thiết bị!");
-    } else if (this.take_over_person == null) {
-      alert("Hãy chọn người bàn giao!");
-    } else if (this.verifier == null) {
-      alert("Hãy chọn người xác nhận !");
+  async updateFixEquipmentRecord() {
+    if (this.fixer == null) {
+      alert("Hãy chọn người sửa thiết bị!");
     } else if (this.editDate === null || this.editDate === undefined) {
       alert("Hãy nhập thời gian bàn giao");
-    } else if (
-      this.record.type_take_over == null ||
-      this.record.type_take_over == ""
-    ) {
+    } else if (this.record.status == null || this.record.status == "") {
       alert("Hãy chọn loại bàn giao");
     }
     // else if (this.msgError!=null){
@@ -469,15 +364,13 @@ export default class UpdateTakeOver extends Vue {
     // }
     else {
       console.log(this.editDate.getTime());
-
       const data = {
         id: this.record.id,
         equipment_id: this.record.equipment_id,
-        username: this.user?.username,
-        take_over_time: this.editDate.getTime(),
-        verifier: this.verifier?.username,
-        take_over_person: this.take_over_person?.username,
-        type_take_over: this.record.type_take_over,
+
+        fixing_time: this.editDate.getTime(),
+        fixer: this.fixer?.username,
+        status: this.record.status,
         message: this.record.message,
         cost: this.record.cost,
         updated_by: "tatthanh",
@@ -486,9 +379,9 @@ export default class UpdateTakeOver extends Vue {
       if (this.record.cost == "") data.cost = null;
 
       console.log(data);
-      TakeOverService.update(data)
+      FixEquipmentService.update(data)
         .then((res) => {
-          alert("Cập nhật thông tin bàn giao thành công !");
+          alert("Cập nhật thông tin sửa chữa thành công !");
           this.changeShow(false);
         })
         .catch((err) => {
